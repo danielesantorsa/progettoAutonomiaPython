aa134b66aa878b079c78c90366a184fb40acb8a5
OscarNardin <114473603+OscarNardin@users.noreply.github.com>
2025-05-08 10:13:18 +0000

    feat: Metodo Start e inizializzo la classe GameServer con i parametri. Gestisco connessione dei due giocatori e creo un thread separato per ogni giocatore per connessione in parallelo

--- server/main.py
+++ server/main.py
@@ -1,22 +1,33 @@
-# Importo le librerie 
 import socket
 import threading
+from .state import GlobalState
+from .UDP_sender import send_UDP_notification
+from shared.protocol import recv_json, send_json
+from .game import GameState
 
-# Inserisco un numero massimo di giocatori per partita
-MAX_PLAYERS = 2
-
-# Permetto che i messaggi possano essere codificati e decodificati in UTF-8
-def encode_message(msg: str) -> bytes:
-    return msg.encode('utf-8')
-    
-
-def decode_message(data: bytes) -> str:
-    return data.decode('utf-8').strip()
-
-# Creo la classe 
-class ServerBattagliaNavale:
-    def __init__(self, host = '127.0.0.1', port = 9999):
-
-        # Configuro indirizzo IP e porta del server
+class GameServer:
+    def __init__(self, host="0.0.0.0", port = 5000):
         self.host = host
         self.port = port
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.global_state = GlobalState()
+        self.lock = threading.Lock()
+        
+    def Start(self):
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(2)
+        print(f"Server in ascolto su: {self.host}:{self.port}")
+
+        for player_id in range(2):
+            # Aspetta che un client si connetta
+            conn, addr = self.server_socket.accept()
+            print(f"Giocatore {player_id} connesso da {addr}")
+
+            # Aggiunge il giocatore alla lista globale
+            self.global_state.add_player(conn, addr) 
+
+            # Manda un messaggio di benvenuto al client con l'ID del giocatore
+            send_json(conn, {"type": "welcome", "player":player_id})
+
+            
+            
